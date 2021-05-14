86129b2490e376a1786f71d2d13182f31db73f35
sek <noreoile@csu.fullerton.edu>
2021-05-14 15:52:53 -0700

    edited

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -17,7 +17,7 @@ class Navbar extends React.Component {
 					<Icon name="list"/>
 				</Button>
 				<a href="/todolist" className="item" id="navbarLink">
-					<div>To Do List edited</div>
+					<div>To Do List edit</div>
 				</a>
 			</div>
 		);
